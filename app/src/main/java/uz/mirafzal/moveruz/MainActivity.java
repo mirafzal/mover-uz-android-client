6721be5c1702487d10440d449b7e351c1c1c9286
mirafzal <shavkatovm99@gmail.com>
2019-04-12 12:44:25 +0500

    add pull to refresh and Toast "check internet connection"

--- app/src/main/java/uz/mirafzal/moveruz/MainActivity.java
+++ app/src/main/java/uz/mirafzal/moveruz/MainActivity.java
@@ -5,18 +5,32 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
 
-    private WebView webview1;
+    private WebView webView;
+    private ProgressBar progressBar;
+    private SwipeRefreshLayout mySwipeRefreshLayout;
+    private ViewTreeObserver.OnScrollChangedListener mOnScrollChangedListener;
+
     private String myurl;
 
     private Intent intent = new Intent();
     private AlertDialog.Builder dialog;
+
+    private boolean isScrollRefresh;
 
 
     @Override
@@ -28,31 +42,33 @@ public class MainActivity extends AppCompatActivity{
     }
 
     private void initialize() {
-        webview1 = findViewById(R.id.webView);
-        webview1.getSettings().setJavaScriptEnabled(true);
-        webview1.getSettings().setSupportZoom(true);
-        webview1.setWebViewClient(new WebViewClient() {
-            @Override
-            public void onPageStarted(WebView _view, final String _url, Bitmap _favicon) {
+        webView = findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setSupportZoom(true);
+        webView.getSettings().setAppCacheEnabled(true);
+        webView.getSettings().setCacheMode(WebSettings.LOAD_DEFAULT);
 
-                super.onPageStarted(_view, _url, _favicon);
-            }
+        mySwipeRefreshLayout = this.findViewById(R.id.swipeContainer);
 
-            @Override
-            public void onPageFinished(WebView _view, final String _url) {
-
-                super.onPageFinished(_view, _url);
-            }
-        });
+        mySwipeRefreshLayout.setOnRefreshListener(
+                new SwipeRefreshLayout.OnRefreshListener() {
+                    @Override
+                    public void onRefresh() {
+                        webView.reload();
+                        isScrollRefresh = true;
+                    }
+                }
+        );
 
         dialog = new AlertDialog.Builder(this);
+
+        progressBar = findViewById(R.id.progressBar);
     }
 
     @Override
-    public void onBackPressed()
-    {
-        if (webview1.canGoBack()) {
-            webview1.goBack();
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
         } else {
             dialog.setIcon(android.R.drawable.ic_dialog_alert)
                     .setTitle("Выход!").setMessage("Вы действительно хотите выйти?")
@@ -66,13 +82,13 @@ public class MainActivity extends AppCompatActivity{
     }
 
     private void initializeLogic() {
-        webview1.loadUrl("https://mover.uz");
+        webView.loadUrl("https://mover.uz");
 
         WebViewClient webViewClient = new WebViewClient() {
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                if (url.contains("watch")) {
-                    if      (url.length() == 32) myurl = url.substring(23, 31);
+                if (url.contains("watch") && !url.contains("comments")) {
+                    if (url.length() == 32) myurl = url.substring(23, 31);
                     else if (url.length() == 31) myurl = url.substring(23, 30);
                     myurl = "https://v.mover.uz/" + myurl + "_m.mp4";
                     intent.setAction(Intent.ACTION_VIEW);
@@ -87,15 +103,60 @@ public class MainActivity extends AppCompatActivity{
 
             @Override
             public void onLoadResource(WebView view, String url) {
-//
+                if (url.contains(".mp4")) {
+                    Toast.makeText(getApplicationContext(), url, Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                if (!isScrollRefresh) {
+                    progressBar.setVisibility(View.VISIBLE);
+                }
             }
 
             @Override
             public void onPageFinished(WebView view, String url) {
                 super.onPageFinished(view, url);
+                progressBar.setVisibility(View.GONE);
+                mySwipeRefreshLayout.setRefreshing(false);
+                isScrollRefresh = false;
             }
+
+            @Override
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                super.onReceivedError(view, errorCode, description, failingUrl);
+                Toast.makeText(getApplicationContext(),
+                        R.string.check_internet_connection,
+                        Toast.LENGTH_LONG).show();
+            }
+
+
         };
-        webview1.setWebViewClient(webViewClient);
+        webView.setWebViewClient(webViewClient);
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        mySwipeRefreshLayout.getViewTreeObserver().addOnScrollChangedListener(mOnScrollChangedListener =
+                new ViewTreeObserver.OnScrollChangedListener() {
+                    @Override
+                    public void onScrollChanged() {
+                        if (webView.getScrollY() == 0)
+                            mySwipeRefreshLayout.setEnabled(true);
+                        else
+                            mySwipeRefreshLayout.setEnabled(false);
+
+                    }
+                });
+    }
+
+    @Override
+    protected void onStop() {
+        mySwipeRefreshLayout.getViewTreeObserver().removeOnScrollChangedListener(mOnScrollChangedListener);
+        super.onStop();
+    }
 }
